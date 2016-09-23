289d35635d719821b184bf0541d9e9d1fb805cd4
Roshan Gonsalkorale <roshan@oracle.com>
2016-09-23 15:24:08 +0100

    BK category export

--- Tampermonkey Scripts/BlueKai Extender.user.js
+++ Tampermonkey Scripts/BlueKai Extender.user.js
@@ -66,8 +66,14 @@
 		var csvContent = "data:text/csv;charset=utf-8,";
 		data.forEach(function(infoArray, index) {
 
-			dataString = infoArray.join(",");
-			csvContent += index < data.length ? dataString + "\n" : dataString;
+			// Replace "," in any data
+			for (var i = 0; i < infoArray.length; i++) {
+				
+				infoArray[i] = infoArray[i].replace(/,/g, "-");
+			}
+
+			var dataString = infoArray.join(",");
+			var csvContent += index < data.length ? dataString + "\n" : dataString;
 
 		});
 
