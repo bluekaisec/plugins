6aaf5d6687ce9d075d184f227c962b0852da39f6
Roshan Gonsalkorale <roshan@oracle.com>
2016-11-09 18:04:34 +0000

    Updating Google Doc link
    -

--- Tampermonkey Scripts/BlueKai Extender_draft.js
+++ Tampermonkey Scripts/BlueKai Extender_draft.js
@@ -1286,7 +1286,7 @@ v1.4 (roshan.gonsalkorale@oracle.com)
 
 			// TO CHANGE : 
 			var message = "<h2>Upload your CSV of NEW categories/rules</h2>" +
-				"<p> (1) <strong><a target='_blank' href='https://drive.google.com/open?id=0B73sA1rCbNo7aE16eTB2YzJrNW8'>Download this template</a></strong> and fill out your category/rule structure</p>" +
+				"<p> (1) <strong><a target='_blank' href='https://docs.google.com/spreadsheets/d/1MLlBUQUO8aJlEVasmSv8YkuzpulCRQ4ncXrXYAhwyek/edit#gid=1626396405'>Download this template</a></strong> and fill out your category/rule structure</p>" +
 				"<p> (2) Upload it to create your new categories/rules</p>" +
 				"<h3> IMPORTANT NOTES!!!</h3>" +				
 				"<li> AFTER IMPORT, CHECK AUDIENCE BUILDER TO SEE IF YOUR CATEGORIES ARE SELECTABLE (if not, fix them with <a target='_blank' href='https://gist.github.com/roshanbluekai/353a3644d6b85e9bd69464390ee4d5f3'><strong>this</strong></a>) </li>" +
